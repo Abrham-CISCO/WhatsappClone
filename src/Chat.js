bc006bc4c11ec7cd14cdb5a900e453b5be4691c9
Abrham Getachew <abrhamcisco@gmail.com>
2021-07-06 16:50:16 -0300

    Group Modal Built

--- src/Chat.js
+++ src/Chat.js
@@ -4,6 +4,7 @@ import db from './firebase'
 import {Avatar, IconButton} from "@material-ui/core"
 import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon';
 import MicIcon from '@material-ui/icons/Mic';
+import AddIcon from '@material-ui/icons/Add';
 import {useParams} from 'react-router-dom';
 import firebase from "firebase";
 import { AttachFile, MoreVert, SearchOutlined } from '@material-ui/icons';
@@ -68,10 +69,59 @@ function Chat() {
                 <Popup trigger={<IconButton><MoreVertIcon /></IconButton>} position="bottom right" modal nested>
                     {close => (
                     <div className="modal">
-                        Content here
                         <button className="close" onClick={close}>
                         &times;
                         </button>
+
+                        <div className = "groupDetail">
+                            <div className = "groupPhName">
+                                <div className = "groupAvatar">
+                                    <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`}/>
+                                </div>
+                            </div>
+                            <div className="groupName">
+                                <div className="name"><h1>{roomName}</h1></div>
+                            </div>
+                            <div className = "groupDescription">
+                                <h3>Description</h3>
+                                <p>This is group description</p>
+                            </div>
+                            <div className = "groupMembers">
+
+                            <h3>Members</h3>
+                                <div className="members">
+
+                                    <div className="member">
+                                        <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`}/>
+                                        <snap className="memberName">Abrham Getachew</snap>
+                                    </div>
+                                    <div className="member">
+                                        <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`}/>
+                                        <snap className="memberName">Abrham Getachew</snap>
+                                    </div>
+                                    <div className="member">
+                                        <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`}/>
+                                        <snap className="memberName">Abrham Getachew</snap>
+                                    </div>
+                                    <div className="member">
+                                        <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`}/>
+                                        <snap className="memberName">Abrham Getachew</snap>
+                                    </div>
+                                    <div className="member">
+                                        <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`}/>
+                                        <snap className="memberName">Abrham Getachew</snap>
+                                    </div>
+                                    <div className="member">
+                                        <div className = "addNewMember">
+                                            <IconButton>
+                                            <AddIcon/>
+                                            </IconButton>
+                                            </div>  
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
                     </div>
 
 
